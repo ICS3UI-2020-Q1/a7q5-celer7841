aa075d34d92f668772212a7292c23bfc0f19a12c
celer7841 <celer7841@wrdsb.ca>
2020-10-15 05:02:43 +0000

    finished

--- Main.java
+++ Main.java
@@ -1,15 +1,22 @@
+import java.util.Scanner;
 /**
  *
  * @author 
  */
 public class Main {
-
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+  firstDigit(3572);
     
+}
+// number =3
+public static void firstDigit(int number){
+  while (number > 10){
+    number = number / 10; //3
   }
+  System.out.println(Math.abs(number));
+}
 }
